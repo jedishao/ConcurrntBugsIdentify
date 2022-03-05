6605288119430e59b3fde3a5f62eeec80ace9890
jedishao23 <shao724854691@163.com>
2022-03-04 21:58:13 -0500

    POS for concurrent bug reports

--- posforConcurrent.py
+++ posforConcurrent.py
@@ -1,8 +1,9 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-conSents = open("./results/concurrentSents.txt")
-pos_results = open("./results/pos_concurrent.txt", "a")
+conSents = open("results/deadlockSents.txt")
+# pos_results = open("./results/pos_concurrent.txt", "a")
+den_results = open("./results/den_concurrent.txt", "a")
 
 sentsList = []
 for line in conSents:
@@ -11,12 +12,19 @@ for line in conSents:
 # doc = nlp("I love dogs")
 # for token in doc:
 #     print(token.pos_)
+# for sent in sentsList:
+#     doc = nlp(sent)
+#     tmplist = []
+#     for token in doc:
+#         tmplist.append(str(token.pos_))
+#     pos_results.write(str(tmplist)+'\n')
+
 for sent in sentsList:
     doc = nlp(sent)
     tmplist = []
     for token in doc:
-        tmplist.append(str(token.pos_))
-    pos_results.write(str(tmplist)+'\n')
+        tmplist.append(str(token.dep_))
+    den_results.write(str(tmplist)+'\n')
 
 conSents.close()
-pos_results.close()
+den_results.close()
