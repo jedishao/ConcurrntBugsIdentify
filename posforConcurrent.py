b75b5dd9a1e26ec48db67f0a8e3f434765697a5b
jedishao23 <shao724854691@163.com>
2022-03-05 20:18:37 -0500

    simple pos

--- posforConcurrent.py
+++ posforConcurrent.py
@@ -1,30 +1,44 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-conSents = open("results/deadlockSents.txt")
-# pos_results = open("./results/pos_concurrent.txt", "a")
-den_results = open("./results/den_concurrent.txt", "a")
 
-sentsList = []
-for line in conSents:
-    sentsList.append(str(line))
+def getTag(nlp, conSents, results):
+    sentsList = []
+    for line in conSents:
+        sentsList.append(str(line))
+    for sent in sentsList:
+        doc = nlp(sent)
+        tmplist = []
+        for token in doc:
+            tmplist.append(str(token.pos_))
+        results.write(str(tmplist) + '\n')
 
-# doc = nlp("I love dogs")
-# for token in doc:
-#     print(token.pos_)
-# for sent in sentsList:
-#     doc = nlp(sent)
-#     tmplist = []
-#     for token in doc:
-#         tmplist.append(str(token.pos_))
-#     pos_results.write(str(tmplist)+'\n')
 
-for sent in sentsList:
-    doc = nlp(sent)
-    tmplist = []
-    for token in doc:
-        tmplist.append(str(token.dep_))
-    den_results.write(str(tmplist)+'\n')
+def getDen(nlp, conSents, results):
+    sentsList = []
+    for line in conSents:
+        sentsList.append(str(line))
+    for sent in sentsList:
+        doc = nlp(sent)
+        tmplist = []
+        for token in doc:
+            tmplist.append(str(token.dep_))
+        results.write(str(tmplist) + '\n')
 
-conSents.close()
-den_results.close()
+
+def main():
+    nlp = spacy.load("en_core_web_sm")
+    conSents = open("results/deadlockSents.txt")
+    pos_results = open("./results/pos_concurrent.txt", "a")
+    den_results = open("./results/den_concurrent.txt", "a")
+
+    getTag(nlp, conSents, pos_results)
+    # getDen(nlp, conSents, den_results)
+
+
+    conSents.close()
+    pos_results.close()
+    den_results.close()
+
+
+if __name__ == '__main__':
+    main()
