906cdb60103861a1aa9b7842d0b1afa3cd6c7d8f
jedishao23 <shao724854691@163.com>
2022-03-06 16:26:09 -0500

    simple pos

--- posforConcurrent.py
+++ posforConcurrent.py
@@ -1,7 +1,7 @@
 import spacy
 
 
-def getTag(nlp, conSents, results):
+def getPos(nlp, conSents, results):
     sentsList = []
     for line in conSents:
         sentsList.append(str(line))
@@ -9,7 +9,12 @@ def getTag(nlp, conSents, results):
         doc = nlp(sent)
         tmplist = []
         for token in doc:
-            tmplist.append(str(token.pos_))
+            if str(token) == 'TUC':
+                tmplist.append("^_^")
+            elif str(token.lemma_) == 'deadlock':
+                tmplist.append("$")
+            else:
+                tmplist.append(str(token.pos_))
         results.write(str(tmplist) + '\n')
 
 
@@ -27,13 +32,12 @@ def getDen(nlp, conSents, results):
 
 def main():
     nlp = spacy.load("en_core_web_sm")
-    conSents = open("results/deadlockSents.txt")
+    conSents = open("results/deadlockSents_NER.txt")
     pos_results = open("./results/pos_concurrent.txt", "a")
     den_results = open("./results/den_concurrent.txt", "a")
 
-    getTag(nlp, conSents, pos_results)
-    # getDen(nlp, conSents, den_results)
-
+    # getPos(nlp, conSents, pos_results)
+    getDen(nlp, conSents, den_results)
 
     conSents.close()
     pos_results.close()
