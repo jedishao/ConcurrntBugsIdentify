03d56fdf8330a9e39e09ba4cc34548efaa9a92f8
jedishao23 <shao724854691@163.com>
2022-03-08 21:37:40 -0500

    pattern 1

--- posforConcurrent.py
+++ posforConcurrent.py
@@ -1,7 +1,16 @@
+import ast
+
 import spacy
 
 
-def getPos(nlp, conSents, results):
+def getPos2file(nlp, conSents, results):
+    """
+
+    :param nlp:
+    :param conSents: target file
+    :param results: results file
+    :return: results
+    """
     sentsList = []
     for line in conSents:
         sentsList.append(str(line))
@@ -10,9 +19,9 @@ def getPos(nlp, conSents, results):
         tmplist = []
         for token in doc:
             if str(token) == 'TUC':
-                tmplist.append("^_^")
+                tmplist.append("TUC")
             elif str(token.lemma_) == 'deadlock':
-                tmplist.append("$")
+                tmplist.append("SYMP")
             else:
                 tmplist.append(str(token.pos_))
         results.write(str(tmplist) + '\n')
@@ -30,14 +39,52 @@ def getDen(nlp, conSents, results):
         results.write(str(tmplist) + '\n')
 
 
+def dependence(nlp, sent):
+    doc = nlp(sent)
+    tmplist = []
+    for token in doc:
+        if str(token.dep_) == 'ROOT':
+            for to in token.childern:
+                if str(to.dep_) == 'nsubj':
+                    print(str(to))
+
+
+def pattern1(sents_with_pos):
+    """
+    :param sents_with_pos: sentences with POS tag
+    :return: the index of sentences that are identified as mode 1
+    """
+
+    sentsList = []
+    for line in sents_with_pos:
+        list_list = ast.literal_eval(line)
+        sentsList.append(list_list)
+    i = 0
+    tmp = []
+    for sens in sentsList:
+        t, v, a = 0, 0, 0  # 'VERB', 'ADV'
+        for token in sens:
+            if str(token) == 'TUC':
+                t += 1
+            elif str(token) == 'VERB' and t > 0:
+                v += 1
+            elif str(token) == 'SYMP' and v > 0:
+                a += 1
+        if a > 0:
+            tmp.append(i)
+        i += 1
+    print(tmp)
+
+
 def main():
     nlp = spacy.load("en_core_web_sm")
     conSents = open("results/deadlockSents_NER.txt")
-    pos_results = open("./results/pos_concurrent.txt", "a")
+    pos_results = open("./results/pos_concurrent.txt")
     den_results = open("./results/den_concurrent.txt", "a")
 
-    # getPos(nlp, conSents, pos_results)
-    getDen(nlp, conSents, den_results)
+    #getPos2file(nlp, conSents, pos_results)
+    pattern1(pos_results)
+    # getDen(nlp, conSents, den_results)
 
     conSents.close()
     pos_results.close()
