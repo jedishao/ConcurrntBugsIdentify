5572ca674a150ebe1780edb225815a4057dc3e86
jedishao23 <shao724854691@163.com>
2022-03-24 20:06:35 -0400

    dataset

--- spider_redisson.py
+++ spider_redisson.py
@@ -38,7 +38,7 @@ def get_content(issue_name):
     # 拼接issue地址
     # 4189
     #url = 'https://github.com/redisson/redisson/issues/' + str(issue_name)
-    url = 'https://github.com/eclipse-vertx/vert.x/issues/' + str(issue_name)
+    url = 'https://github.com/grpc/grpc-java/issues/' + str(issue_name)
     response = requests.get(url)
     # if str(response) == '<Response [404]>':
     page_source = response.text
@@ -66,9 +66,9 @@ def get_content(issue_name):
 
 
 if __name__ == '__main__':
-    with open(r'./results/projects/grpc.txt', 'a+', encoding='utf-8') as f:
+    with open(r'./results/projects/grc.txt', 'a+', encoding='utf-8') as f:
         #repo = 'redisson/redisson'
-        repo = 'eclipse-vertx/vert.x'
+        repo = 'grpc/grpc-java'
         # 拼接项目url
         repos_url = 'https://github.com/' + repo
         # print(repos_url)
@@ -80,15 +80,15 @@ if __name__ == '__main__':
         # f.write(str(number))
         # f.write('\n')
         issues_list = []
-        for i in range(1836):
-            i = i + 2487
+        for i in range(1000):
+            i = i + 1
             issues_list.append(str(i))
         # 格式：/combust/mleap/issues/716
         for issue in issues_list:
             # 获取issue的内容
             #issue_url = 'https://github.com/redisson/redisson/issues/' + issue
-            issue_url = 'https://github.com/eclipse-vertx/vert.x/issues/' + issue
-            # issue_url = 'https://github.com/grpc/grpc-java/issues/' + issue
+            #issue_url = 'https://github.com/eclipse-vertx/vert.x/issues/' + issue
+            issue_url = 'https://github.com/grpc/grpc-java/issues/' + issue
             title, content = get_content(issue)
             # content=filter_emoji(content)
             # if content is None:
