3e5a1266ef0bd63fe596cbbb4fa9e051a10c89bc
jedishao23 <shao724854691@163.com>
2022-03-26 22:34:43 -0400

    dataset

--- spider_redisson.py
+++ spider_redisson.py
@@ -37,7 +37,7 @@ def get_404():
 def get_content(issue_name):
     # 拼接issue地址
     # 4189
-    #url = 'https://github.com/redisson/redisson/issues/' + str(issue_name)
+    # url = 'https://github.com/redisson/redisson/issues/' + str(issue_name)
     url = 'https://github.com/grpc/grpc-java/issues/' + str(issue_name)
     response = requests.get(url)
     # if str(response) == '<Response [404]>':
@@ -65,12 +65,35 @@ def get_content(issue_name):
     return issue_title, issue_content
 
 
+def get_hsqldbContent(issue_name):
+    # 拼接issue地址
+    # 4189
+    url = 'https://sourceforge.net/p/hsqldb/bugs/' + str(issue_name)
+    response = requests.get(url)
+    # if str(response) == '<Response [404]>':
+    page_source = response.text
+    tree = etree.HTML(page_source)
+    if len(tree.xpath('//title')) > 0:
+        issue_title = tree.xpath('//title')[0].xpath('string(.)')
+    else:
+        issue_title = []
+
+        # 获取issue内容
+    if len(tree.xpath('//div[@id="ticket_content"]')) != 0:
+        issue_content = tree.xpath('//div[@id="ticket_content"]')[0].xpath('string(.)')
+    else:
+        issue_content = []
+
+    return issue_title, issue_content
+
+
 if __name__ == '__main__':
-    with open(r'./results/projects/grc.txt', 'a+', encoding='utf-8') as f:
-        #repo = 'redisson/redisson'
-        repo = 'grpc/grpc-java'
+    with open(r'results/projects/hsqldb_data_400.txt', 'a+', encoding='utf-8') as f:
+        # repo = 'redisson/redisson'
+        # repo = 'grpc/grpc-java'
         # 拼接项目url
-        repos_url = 'https://github.com/' + repo
+        # repos_url = 'https://github.com/' + repo
+        repos_url = 'https://sourceforge.net/p/hsqldb/bugs/'
         # print(repos_url)
         f.write('\n\n')
         f.write(repos_url)
@@ -80,16 +103,17 @@ if __name__ == '__main__':
         # f.write(str(number))
         # f.write('\n')
         issues_list = []
-        for i in range(1000):
+        for i in range(400):
             i = i + 1
             issues_list.append(str(i))
         # 格式：/combust/mleap/issues/716
         for issue in issues_list:
             # 获取issue的内容
-            #issue_url = 'https://github.com/redisson/redisson/issues/' + issue
-            #issue_url = 'https://github.com/eclipse-vertx/vert.x/issues/' + issue
-            issue_url = 'https://github.com/grpc/grpc-java/issues/' + issue
-            title, content = get_content(issue)
+            # issue_url = 'https://github.com/redisson/redisson/issues/' + issue
+            # issue_url = 'https://github.com/eclipse-vertx/vert.x/issues/' + issue
+            # issue_url = 'https://github.com/grpc/grpc-java/issues/' + issue
+            issue_url = 'https://sourceforge.net/p/hsqldb/bugs/' + issue
+            title, content = get_hsqldbContent(issue)
             # content=filter_emoji(content)
             # if content is None:
             #     print('S')
