f0cde7a12d44993800a44afd875786a108d39a8a
jedishao23 <shao724854691@163.com>
2022-03-27 22:43:51 -0400

    dataset

--- corpus.py
+++ corpus.py
@@ -6,6 +6,8 @@ TMP = ['twice', 'again', 'forever', 'never', 'already']
 
 SYMP = ['deadlock', 'stick', 'stuck', 'hang']
 
+STE = ['thread', 'lock', 'transcation']
+
 NUM = []
 
 OBJ = ['dobj', 'conj']
@@ -13,3 +15,5 @@ OBJ = ['dobj', 'conj']
 S = ['nsubj', 'nsubjpass']
 
 ADV = ['advmod', 'amod']
+
+ALL = ['invoke', 'lock', 'cause', 'block', 'acquire', 'trigger', 'release', 'wait', 'load', 'involve']
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils import utils
 
 def main():
     nlp = spacy.load("en_core_web_sm")
-    conSents = open("results/deadlockSents_NER.txt")
+    conSents = open("results/projects/original.txt")
     # only_one = open('results/only_one_cmi.txt')
     write_pos = open("./results/pos_concurrent.txt", "w")
     read_pos = open("./results/pos_concurrent.txt")
@@ -16,25 +16,25 @@ def main():
     # file-->pos-->pattern1(SVO)-->targetFile(sents)-->dependence
     # dataset = getDataset2list(conSents)
     dataset = IOutils.getDataset2list(conSents)
-    # IOutils.getPos2file(nlp, dataset, write_pos)
-    # write_pos.close()
-    CVAS_set, C_set = pattern_1.getPtn1byKw(nlp, dataset)
-    # sent_cva_set = utils.getSentsByIndex(dataset, CVA_index)
-    # sent_cvsy_set = utils.getSentsByIndex(dataset, CVSY_index)
-    # for s in sent_cvsy_set:
-    #     print(s, end="")
-    ptn1_results = pattern_1.getSentsOfPtn1(nlp, CVAS_set)
-    # listd = ['CMI should not cause deadlock.']
-    ptn12_results = pattern_1.getSentsOfPtn1_2(nlp, CVAS_set)
-    print(len(CVAS_set))
-    print(len(C_set))
+    IOutils.getPos2file(nlp, dataset, write_pos)
+    write_pos.close()
+    test_results = pattern_1.testKW(nlp, dataset)
+    # CVA_index, CVSY_index = pattern_1.getPtn1byPos(read_pos)
+    # CVA_dataset = utils.getSentsByIndex(dataset, CVA_index)
+    # CVSY_dataset = utils.getSentsByIndex(dataset, CVSY_index)
+    # CVA_set = pattern_1.getPtn1byKw_CVA(nlp, CVA_dataset)
+    # CVSY_set = pattern_1.getPtn1byKw_CVSY(nlp, CVSY_dataset)
+
+    print(test_results)
+
     # for sent in CVAS_set:
-    #     print(sent, end="")
+    #     print(dataset[sent], end="")
 
-    print('\n'+'==============================================')
-
-    for sent in C_set:
-        print(sent, end="")
+    print('\n' + '==============================================')
+    # print(CVSY_index)
+    # for sent in C_set:
+    #     print(sent)
+    # print(dataset[sent], end="")
 
     conSents.close()
     # only_one.close()
