5fbf913548b66c79c599ad49fd4c18b7d461e9cb
jedishao23 <shao724854691@163.com>
2022-04-04 17:59:12 -0400

    corpus

--- corpus.py
+++ corpus.py
@@ -4,13 +4,14 @@ OTHER = ['be', 'experience', 'lead', 'confirm', 'happen', 'test', 'see', 'give',
 
 TMP = ['twice', 'again', 'forever', 'never', 'already']
 
-SYMP = ['deadlock', 'stick', 'stuck', 'hang']
+SYMP = ['deadlock', 'stick', 'stuck', 'hang', 'block']
 
-EXC = ['exception', 'error']
+EXC = ['exception', 'Exception', 'error', 'Error', 'EXC']
 
-STE = ['thread', 'lock', 'transcation', 'race']
+STE = ['thread', 'lock', 'transcation', 'race', 'concurrence', 'Concurrence', 'concurrency', 'Concurrency',
+       'concurrent', 'Concurrent']
 
-COB = ['race', 'deadlock']
+COB = ['race', 'deadlock', 'Deadlock', 'livelock']
 
 NUM = []
 
@@ -21,3 +22,5 @@ S = ['nsubj', 'nsubjpass']
 ADV = ['advmod', 'amod']
 
 ALL = ['invoke', 'lock', 'cause', 'block', 'acquire', 'trigger', 'release', 'wait', 'load', 'involve']
+
+CST = ['screen']
