01f319a3ebd815936b3b84d58628448a3c3133b4
jedishao23 <shao724854691@163.com>
2022-04-04 17:48:51 -0400

    pattern 2

---
+++ patterns/pattern_2.py
@@ -0,0 +1,21 @@
+# @Time    : 4/3/22 1:42 PM
+# @Author  : Shuai S
+# @File    : pattern_2.py
+import corpus
+
+
+def check(nlp, dataset):
+    for line in dataset:
+        doc = nlp(line)
+        for token in doc:
+            # constraint (race condition)
+            if str(token.lemma_) == 'race':
+                if str(token.head) == 'condition':
+                    print(token)
+                    return True
+    return False
+
+
+def constraint(doc):
+    print()
+
