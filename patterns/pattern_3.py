88bc07617288391a5918c18f66e872955628c9b4
jedishao23 <shao724854691@163.com>
2022-04-05 17:12:50 -0400

    pattern 3

--- patterns/pattern_3.py
+++ patterns/pattern_3.py
@@ -6,18 +6,29 @@ import corpus
 
 def check(nlp, dataset):
     for line in dataset:
-        cmi, neg = False, False
+        cmi, adv, neg1, neg2 = False, False, False, False
         doc = nlp(line)
         for token in doc:
-            # constraint (race condition)
             if str(token.dep_) == 'ROOT':
-                if str(token.lemma_) == 'work':
+                if str(token.lemma_) in ['work', 'unlock']:
                     for child in token.children:
                         # print(str(child)+'--->'+str(child.dep_))
-                        # if str(child) == 'CMI' and str(child.dep_) in corpus.S:
-                        #     cmi = True
+                        if str(child) == 'CMI' and str(child.dep_) in corpus.S:
+                            cmi = True
                         if str(child.lemma_) == 'not' and str(child.dep_) == 'neg':
-                            neg = True
-        if neg and cmi:
-            return True
+                            neg1 = True
+                else:
+                    for child in token.children:
+                        # print(str(child)+'--->'+str(child.dep_))
+                        if str(child) == 'CMI' and str(child.dep_) in corpus.S:
+                            cmi = True
+                        if str(child.lemma_) == 'not' and str(child.dep_) == 'neg':
+                            neg2 = True
+                        if str(child.lemma_) in ['correctly', 'properly'] and str(child.dep_) in corpus.ADV:
+                            adv = True
+        if cmi:
+            if neg1:
+                return True
+            if neg2 and adv:
+                return True
     return False
