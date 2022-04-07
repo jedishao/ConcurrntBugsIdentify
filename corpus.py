46a324fd566b146831bd4bee8cbbce474dd6de62
jedishao23 <shao724854691@163.com>
2022-04-07 10:39:08 -0400

    corpus

--- corpus.py
+++ corpus.py
@@ -1,4 +1,5 @@
-COP = ['invoke', 'lock', 'cause', 'block', 'acquire', 'trigger', 'release', 'wait', 'load', 'involve', 'interrupt']
+COP = ['invoke', 'lock', 'cause', 'block', 'acquire', 'trigger', 'release', 'wait', 'load', 'involve', 'interrupt',
+       'unlock', 'unlocked']
 
 OTHER = ['be', 'experience', 'lead', 'confirm', 'happen', 'test', 'see', 'give', 'enter', 'complete', 'make']
 
@@ -6,19 +7,19 @@ TMP = ['twice', 'again', 'forever', 'never', 'already', 'indefinitely']
 
 SYMP = ['deadlock', 'stick', 'stuck', 'hang', 'block', 'remain', 'hold', 'stall']
 
-EXC = ['exception', 'Exception', 'error', 'Error', 'EXC']
+EXC = ['exception', 'error', 'exc', 'npe']
 
-STE = ['thread', 'lock', 'transcation', 'race', 'concurrence', 'Concurrence', 'concurrency', 'Concurrency',
-       'concurrent', 'Concurrent']
+STE = ['thread', 'lock', 'transcation', 'race', 'concurrence', 'concurrency', 'concurrent']
 
-LOG = ['trace', 'log', 'S_T_A']
+LOG = ['trace', 'log', 's_t_a']
 
 ISS = ['issue', 'problem', 'incorrect']
 
-KW = ['thread', 'lock', 'transcation', 'race', 'concurrence', 'Concurrence', 'concurrency', 'Concurrency',
-      'concurrent', 'Concurrent', 'CMI']
+KW = ['thread', 'lock', 'transcation', 'race', 'concurrence', 'concurrency', 'concurrent', 'cmi', 'order']
 
-COB = ['race', 'deadlock', 'Deadlock', 'livelock']
+COB = ['race', 'deadlock', 'livelock']
+
+TER = ['thsa', 'reentrant']  # thsa: thread safe
 
 NUM = []
 
