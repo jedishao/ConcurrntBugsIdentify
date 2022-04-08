67ea8f2e0b036d352ecc85b83f354f559afa2050
jedishao23 <shao724854691@163.com>
2022-04-08 11:28:15 -0400

    pattern 5

--- patterns/pattern_5.py
+++ patterns/pattern_5.py
@@ -1,6 +1,8 @@
 # @Time    : 4/5/22 3:07 PM
 # @Author  : Shuai S
 # @File    : pattern_5.py
+import re
+
 import corpus
 
 
@@ -9,7 +11,7 @@ def check(nlp, dataset):
         doc = nlp(line)
         for token in doc:
             if str(token.dep_) == 'ROOT':
-                if str(token.lemma_) in corpus.ISS:
+                if str(token.lemma_).lower() in corpus.ISS:
                     for child in token.children:
                         if str(child) == 'CMI':
                             return True
@@ -17,4 +19,21 @@ def check(nlp, dataset):
                             for grandchild in child.children:
                                 if str(grandchild) == 'CMI':
                                     return True
+                        elif str(child.dep_) == 'prep':
+                            for grandchild in child.children:
+                                if str(grandchild) == 'CMI':
+                                    return True
+                else:
+                    for child in token.children:
+                        if str(child) == 'CMI':
+                            return True
+                        elif str(child.lemma_).lower() in corpus.ISS:
+                            for grandchild in child.children:
+                                if str(grandchild.lemma_).lower() in corpus.COB:
+                                    return True
+                        elif str(child.dep_) == 'compound':
+                            if str(child) == 'CMI':
+                                for grandchild in child.children:
+                                    if str(grandchild.lemma_).lower() in corpus.ISS:
+                                        return True
     return False
