6fddf637491b760f58e133c7884a71f2429b7898
jedishao23 <shao724854691@163.com>
2022-04-08 11:27:12 -0400

    pattern 3

--- patterns/pattern_3.py
+++ patterns/pattern_3.py
@@ -1,34 +1,51 @@
 # @Time    : 4/3/22 1:43 PM
 # @Author  : Shuai S
 # @File    : pattern_3.py
+import re
+
 import corpus
 
 
 def check(nlp, dataset):
     for line in dataset:
-        cmi, adv, neg1, neg2 = False, False, False, False
+        cmi, adv, neg1, neg2, neg3, ter, cop = False, False, False, False, False, False, False
         doc = nlp(line)
         for token in doc:
             if str(token.dep_) == 'ROOT':
                 if str(token.lemma_) in ['work', 'unlock']:
                     for child in token.children:
-                        # print(str(child)+'--->'+str(child.dep_))
                         if str(child) == 'CMI' and str(child.dep_) in corpus.S:
                             cmi = True
                         if str(child.lemma_) == 'not' and str(child.dep_) == 'neg':
                             neg1 = True
                 else:
+                    # CMI is not reentrant.
+                    # CMI cannot be correctly unlocked.
                     for child in token.children:
-                        # print(str(child)+'--->'+str(child.dep_))
                         if str(child) == 'CMI' and str(child.dep_) in corpus.S:
                             cmi = True
                         if str(child.lemma_) == 'not' and str(child.dep_) == 'neg':
                             neg2 = True
-                        if str(child.lemma_) in ['correctly', 'properly'] and str(child.dep_) in corpus.ADV:
-                            adv = True
+                        if str(child.lemma_) in ['correctly', 'properly']:
+                            if str(child.dep_) in corpus.ADV:
+                                adv = True
+                        if re.search('comp', str(child.dep_)):
+                            if str(child.lemma_).lower() in corpus.COP:
+                                cop = True
+                            else:
+                                for grandchild in child.children:
+                                    if str(grandchild.lemma_) == 'not' and str(grandchild.dep_) == 'neg':
+                                        neg3 = True
+                                    elif str(grandchild.lemma_) in corpus.TER and str(grandchild.dep_) == 'attr':
+                                        ter = True
         if cmi:
             if neg1:
                 return True
-            if neg2 and adv:
-                return True
+            if neg2:
+                if ter or adv:
+                    return True
+                if cop:
+                    return True
+        if neg3 and ter:
+            return True
     return False
