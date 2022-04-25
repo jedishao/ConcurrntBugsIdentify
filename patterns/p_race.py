bde4e0089ebee924d44d27c89fd1d1bdb5a34459
jedishao23 <shao724854691@163.com>
2022-04-24 20:00:37 -0400

    pattern refactoring

---
+++ patterns/p_race.py
@@ -0,0 +1,30 @@
+# @Time    : 4/23/22 9:13 PM
+# @Author  : Shuai S
+# @File    : p_race.py
+
+import spacy
+
+import corpus
+# The same lock name was likely concurrently obtained and held by another thread possibly on another jvm.
+
+te = open('test.txt')
+lineList = []
+nlp = spacy.load("en_core_web_sm")
+for li in te:
+    lineList.append(li)
+
+
+def check(line):
+    doc = nlp(line)
+    sam, mec = False, False
+    for token in doc:
+        if str(token.dep_) == 'ROOT':
+            for child in token.children:
+                if str(child.dep_) == 'amod':
+                    if str(child.lemma_) in corpus.MEC:
+                        return 'p15'
+
+
+for lii in lineList:
+    s = check(lii)
+    print(s)
---
+++ patterns/p_work.py
@@ -0,0 +1,49 @@
+# @Time    : 4/23/22 5:39 PM
+# @Author  : Shuai S
+# @File    : p_work.py
+import spacy
+
+# but when i make these MEC from different Threads it does not works.
+# RLock did not work.
+# ReadLock not working correctly.
+# ReadWriteLock not working.
+# `redissonClient.getRedLock` does not work well with Read-Write block in a multi-JVM environment.
+# ElasticacheCluster not working correctly with DistributedLocks.
+# The RedissonMultiLock works fine if all the redis nodes are alive.
+# ReadWriteLock timeout does not work as expected.
+# Same scenario works as expected when there is no exception or error inside the try block.
+
+
+import corpus
+
+te = open('test.txt')
+lineList = []
+nlp = spacy.load("en_core_web_sm")
+for li in te:
+    lineList.append(li)
+
+
+def check(line):
+    doc = nlp(line)
+    for token in doc:
+        if str(token.dep_) == 'ROOT':
+            for child in token.children:
+                if str(child.dep_) == 'neg':
+                    for child1 in token.children:
+                        if str(child1.dep_) == 'advcl':
+                            for grandchild in child1.children:
+                                if str(grandchild.lemma_) in corpus.MEC:
+                                    return 'P3'
+                        elif str(child1.dep_) in corpus.S:
+                            if str(child1.lemma_) in corpus.MEC:
+                                return 'P4'
+                            else:
+                                for grandchild in child1.children:
+                                    if str(grandchild.lemma_) in corpus.MEC:
+                                        return 'P5'
+                        elif str(child1.dep_) == 'prep':
+                            for grandchild in child1.children:
+                                if str(grandchild.lemma_) in corpus.MEC:
+                                    return 'P6'
+
+
