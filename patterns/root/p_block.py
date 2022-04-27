fb7e95031e8cb407e3d8baaa45fa9af99cd569de
jedishao23 <shao724854691@163.com>
2022-04-27 15:07:32 -0400

    pattern refactoring

---
+++ patterns/root/p_block.py
@@ -0,0 +1,30 @@
+# @Time    : 4/27/22 2:50 PM
+# @Author  : Shuai S
+# @File    : p_block.py
+import spacy
+
+import corpus
+
+# RLock trylock blocks forever.
+
+te = open('../test.txt')
+lineList = []
+nlp = spacy.load("en_core_web_sm")
+for li in te:
+    lineList.append(li)
+
+
+def check(line):
+    doc = nlp(line)
+    for token in doc:
+        if str(token.lemma_) == 'block':
+            for child in token.children:
+                if str(child.dep_) == 'prep':
+                    for grandchild in child.children:
+                        if str(grandchild.lemma_) in corpus.MEC:
+                            return 'P41'
+
+
+for lii in lineList:
+    s = check(lii)
+    print(s)
