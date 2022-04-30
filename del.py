9ba6dd386c87d6f5b14ca4b51e781cb31b22dc09
jedishao23 <shao724854691@163.com>
2022-04-30 00:11:54 -0400

    pattern refactoring

--- del.py
+++ del.py
@@ -1,10 +1,31 @@
 # @Time    : 4/2/22 8:23 PM
 # @Author  : Shuai S
 # @File    : del.py
-import labelOfdata
+import spacy
 
-re = []
-for c in labelOfdata.redisson_exc:
-    if c not in labelOfdata.ste_exc:
-        re.append(c)
-print(labelOfdata.redisson_exc)
+nlp = spacy.load("en_core_web_sm")
+fil = open('dataset/dataset1/redisson/redisson_neg.txt', encoding='utf-8')
+
+li = []
+for line in fil:
+    li.append(line)
+lp = []
+j = 0
+for l in li:
+    doc = nlp(l)
+    for d in doc.sents:
+        lp.append(str(d))
+    for lll in lp:
+        doc = nlp(lll)
+        key = 0
+        for token in doc:
+            if str(token.lemma_) in ['compete', 'atomic', 'concurrency', 'synchronization', 'race', 'deadlock',
+                                     'deadlocked', 'concurrent',
+                                     'mutex', 'lock']:
+                key = 1
+    if key == 1:
+        j += 1
+    lp = []
+
+
+print(j)
