ad8cbcaed12e28f9619ae65bec65a20f04c41fd3
jedishao23 <shao724854691@163.com>
2022-05-22 16:59:04 -0400

    patterns refactoring

--- sents.py
+++ sents.py
@@ -25,14 +25,44 @@ for re in linelist:
 print(len(li))
 
 lsls = []
+pos = []
+neg = []
+p = 0
 for ll in li:
     doc = nlp(ll)
     for token in doc:
-        if str(token.pos_) == 'NOUN':
-            if not lsls.__contains__(str(token.lemma_)):
-                lsls.append(str(token.lemma_))
-print(len(lsls))
-print(lsls)
+        if str(token.lemma_) in ['race', 'condition', 'deadlock', 'threadsafe', 'stravation', 'exception', 'problem',
+                                 'failure', 'issue', 'error', 'dump', 'stack', 'trace', 'inconsistency', 'crash',
+                                 'fail', 'invaild', 'violation' 'thread', 'transaction', 'writelock', 'lock',
+                                 'readlock', 'threadpool', 'rwlock', 'monitor', 'multilock', 'semaphore',
+                                 'distributedlock',
+                                 'readwritelock', 'behavior', 'synchronization', 'order', 'block', 'atomic',
+                                 'concurrency', 'parallel', 'reentrancy', 'reentrant', 'atomicity', 'concurrence',
+                                 'hold', 'create', 'make',
+                                 'obtain', 'call', 'execute', 'acquire', 'lock', 'remain', 'synchronize', 'invoke',
+                                 'attempt', 'request',
+                                 'involve', 'unlock', 'require', 'monitor', 'sync', 'shutdown', 'stop', 'fail',
+                                 'terminate', 'dump', 'block', 'wait', 'shut', 'collapse', 'keep', 'race', 'hang',
+                                 'await', 'park', 'crash', 'stick', 'break', 'suspect',
+                                 'corrupt', 'stuck', 'stall', 'disable', 'sink', 'destroy', 'retain', 'maintain',
+                                 'sustain', 'interrupt', 'freeze', 'persist', 'properly', 'never', 'insufficiently',
+                                 'correctly', 'incorrectly', 'always', 'only', 'then', 'just', 'concurrently', 'yet',
+                                 'already', 'forever', 'long time', 'indefinitely',
+                                 'consistently', 'synchronously', 'rarely', 'again', 'merely', 'constantly',
+                                 'infinitely' 'twice', 'explicitly', 'on', 'during', 'of', 'in', 'for', 'with', 'as',
+                                 'at', 'until', 'to', 'from',
+                                 'between', 'by', 'against', 'without' 'before', 'within', 'under', 'into', 'out',
+                                 'via', 'up', 'about', 'after', 'onto', 'over', 'towards', 'along', 'among']:
+            p = 1
+            pos.append(ll)
+            break
+    if p == 0:
+        neg.append(ll)
+    else:
+        p = 0
+
+print(len(pos))
+print(len(neg))
 # ji = 0
 # for token in li:
 #     doc = nlp(token)
